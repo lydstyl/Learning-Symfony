45997d8b9391cb8720a19c165435a322411c7af6
Gabriel Brun <lydstyl@gmail.com>
2017-06-24 15:26:56 +0200

    ajout de bye bye world working

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -14,4 +14,9 @@ class AdvertController extends Controller
         $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Gabriel'));
         return new Response($content);
     }
+    public function byebyeAction()
+    {
+        $content = $this->get('templating')->render('OCPlatformBundle:Advert:bye.html.twig', array('bigName' => 'Brun'));
+        return new Response($content);        
+    }
 }
